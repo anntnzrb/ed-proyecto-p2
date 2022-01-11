ecd247675446e3f7a60b608a81259acc313ef5f4
anntnzrb <anntnzrb@protonmail.com>
2022-01-11 18:22:51 -0500

    Cambio Tree

--- src/main/java/ec/edu/espol/proyecto/ds/Tree.java
+++ src/main/java/ec/edu/espol/proyecto/ds/Tree.java
@@ -1,128 +1,31 @@
 package ec.edu.espol.proyecto.ds;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public final class Tree<E> {
-
-    private TreeNode<E> root;
+    private final E             data;
+    private final Heap<Tree<E>> children;
+    private final Comparator<E> cmp;
 
     /* constructores */
-    public Tree(E board) {
-        root = new TreeNode(board);
+    Tree(final E data, final Comparator<E> cmp) {
+        this.data = data;
+        this.cmp = cmp;
+        children = new Heap<>((o1, o2) -> cmp.compare(o1.getData(), o2.getData()));
     }
 
-    public TreeNode<E> getRoot() {
-        return root;
+    public Tree<E> add(final E data) {
+        children.offer(new Tree<>(data, cmp));
+
+        return this;
     }
 
-    public void setRoot(final TreeNode<E> root) {
-        this.root = root;
+    /* getters & setters */
+    public E getData() {
+        return data;
     }
 
-    public TreeNode<E> findNode(E data) {
-        return findNode(data, root);
+    public Heap<Tree<E>> getChildren() {
+        return children;
     }
-
-    private TreeNode<E> findNode(E data, TreeNode<E> treeNode) {
-        if (treeNode == null) {
-            return null;
-        }
-        if ((treeNode.getData()).equals(data)) {
-            return treeNode;
-        }
-
-        for (TreeNode<E> nod : treeNode.getChildren()) {
-            TreeNode<E> nL = findNode(data, nod);
-            if (nL != null) {
-                return nL;
-            }
-        }
-        return null;
-    }
-
-    public List<E> getUtilidadMax(final E data) {
-        TreeNode<E> treeNode = this.findNode(data);
-        if (treeNode == null) {
-            return null;
-        }
-        List<E> lData = new LinkedList<>();
-        treeNode.getChildren().forEach((nd) -> {
-            lData.add(nd.getData());
-        });
-        return lData;
-    }
-
-    public boolean add(E parent, E child) {
-        TreeNode<E> TreeNode = findNode(child);
-        if (TreeNode != null) {
-            return false;
-        }
-        TreeNode = new TreeNode<>(child);
-        if (root == null && parent == null) {
-            this.root = TreeNode;
-            return true;
-        }
-
-        TreeNode<E> parentNode = findNode(parent);
-        if (parentNode != null) {
-            parentNode.addChild(TreeNode);
-            return true;
-        }
-
-        return false;
-    }
-
-    //__________________________________________________________
-    public class TreeNode<E> {
-
-        private E data;
-        private List<TreeNode<E>> children;
-
-        /* constructores */
-        public TreeNode(final E data) {
-            this.data = data;
-            this.children = new ArrayList<>();
-        }
-
-        public TreeNode(TreeNode<E> node) {
-            this.data = (E) node.getData();
-            children = new ArrayList<>();
-        }
-
-        /* getters & setters */
-        public E getData() {
-            return data;
-        }
-
-        List<TreeNode<E>> getChildren() {
-            return children;
-        }
-
-        public void addChild(TreeNode<E> child) {
-            if (data == null) {
-                this.data = child.data;
-            } else {
-                children.add(child);
-            }
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (null == obj) {
-                return false;
-            }
-            if (obj instanceof TreeNode) {
-                if (((TreeNode<?>) obj).getData().equals(this.data)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
 }
